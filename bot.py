f68e770e1a9832402876c049dbf13adcfab4a38c
jabsquake123 <jabsquake123@gmail.com>
2025-06-28 15:48:00 +0100

    Update bot.py with latest changes

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 
 
 # Product URL and headers
-PRODUCT_URL = 'PRODUCT_URL = 'https://www.popmart.com/gb/products/1159/SKULLPANDA-Aisling-Figure'
+PRODUCT_URL = 'https://www.popmart.com/gb/products/1159/SKULLPANDA-Aisling-Figure'
 HEADERS = {
     'User-Agent': 'Mozilla/5.0'
 }
